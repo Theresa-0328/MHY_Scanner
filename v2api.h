01841785aaa812963d58e0e8d46149ffb5c4b325
Theresa <101574559+Theresa-0328@users.noreply.github.com>
2023-02-18 17:12:41 +0800

    Add files via upload
    Complete download module

--- v2api.h
+++ v2api.h
@@ -5,13 +5,13 @@ class v2api
 {
 public:
 	v2api();
-	void Initialize();
+	std::string Initialize();
 	std::string GetHttpStream(std::string api, std::map<std::string, std::string> param);
 private:
 	int GetRealRoomID();
 	int HandlerLiveStatus(std::string string);
 private:
-	const std::string V1API = "https://api.live.bilibili.com/xlive/web-room/v1/playUrl/playUrl";
+	//const std::string V1API = "https://api.live.bilibili.com/xlive/web-room/v1/playUrl/playUrl";
 	const std::string V2API = "https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo";
 	const std::string address = "https://api.live.bilibili.com/room/v1/Room/room_init";
 	utils utils;
