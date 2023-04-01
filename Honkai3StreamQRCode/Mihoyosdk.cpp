f2bc48198b059067b48185f5221ef68d8bf18eb4
Theresa-0328 <Theresa-Apocalypse@outlook.com>
2023-04-01 23:56:41 +0800

    sha256

--- Honkai3StreamQRCode/Mihoyosdk.cpp
+++ Honkai3StreamQRCode/Mihoyosdk.cpp
@@ -10,6 +10,34 @@ std::string Mihoyosdk::verify(std::string uid, std::string access_key)
 	json::Json body;
 	body.parse(verifyBody);
 	body["data"] = data;
-	std::cout << body.str() << std::endl;
+	//std::cout << body.str() << std::endl;
+	makeSign(body.str());
 	return std::string();
+}
+
+std::string Mihoyosdk::makeSign(std::string data)
+{
+	std::string sign;
+	std::string	data2;
+	json::Json p;
+	p.parse(data);
+	std::map<std::string,std::string> m= p.objToMap();
+	for (auto &it: m)
+	{
+		if (it.first == "sign")
+			continue;
+		data2 += it.first + "=" + it.second + "&";
+	}
+	data2.erase(data2.length() - 1);
+	std::cout << data2 << std::endl;
+	sign = bh3Sign(data2);
+	return std::string();
+}
+
+std::string Mihoyosdk::bh3Sign(std::string data)
+{
+	std::string key = "0ebc517adb1b62c6b408df153331f9aa";
+	std::string sign =
+	
+	return sign;
 }
