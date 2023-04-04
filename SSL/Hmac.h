04619a214b85389bf748d3cce982ec9aea9b2194
Theresa-0328 <Theresa-Apocalypse@outlook.com>
2023-04-05 04:47:00 +0800

    hmac_sha256

--- SSL/Hmac.h
+++ SSL/Hmac.h
@@ -1,6 +1,19 @@
 #pragma once
+#include <string>
+#include "Sha256.h"
+
+#define INNER_PAD '\x36'
+#define OUTER_PAD '\x5c'
 class Hmac
 {
-
+public:
+	Hmac();
+	std::string hmac_sha256(std::string key, std::string message);
+	void init();
+	void updata();
+	void final();
+private:
+	std::string key;
+	std::string message;
+	std::string output;
 };
-
