6bb723757f04907a9717554aef22b53766aaacbb
Theresa-0328 <Theresa-Apocalypse@outlook.com>
2023-04-06 22:35:23 +0800

    update
    path

--- src/SSL/hmac_sha256.cpp
+++ src/SSL/hmac_sha256.cpp
@@ -30,32 +30,32 @@ std::string hmac_sha256(const std::string& secret, const std::string& indata)
 	return ss.str();
 }
 
-//int main()
-//{	
-//	uint8_t outdata[128] = { 0 };
-//	uint8_t md[SHA256_DIGESTLEN] = { 0 };
-//	//密钥
-//	uint8_t secret[] = { "0ebc517adb1b62c6b408df153331f9aa" };
-//	//数据
-//	uint8_t indata[] = { "app_id=1&channel_id=14&data={\"uid\":96023077,\"access_key\":\"ce03007dae8fec024b8482a08e8dda94_sh\"}&device=0000000000000000" };
-//	int len1 = strlen((char*)secret);
-//	int len2 = strlen((char*)indata);
-//
-//	HMAC_SHA256_CTX hmac;
-//	hmac_sha256_init(&hmac, secret, len1);
-//	hmac_sha256_update(&hmac, indata, len2);
-//	hmac_sha256_final(&hmac, md);
-//	memcpy(outdata, md, SHA256_DIGESTLEN);
-//
-//	for (int i = 0; i < SHA256_DIGESTLEN; i++)
-//	{
-//		printf("%02x", outdata[i]);
-//	}
-//	printf("\n");
-//
-//
-//	std::string s = "0ebc517adb1b62c6b408df153331f9aa";
-//	std::string i = "app_id=1&channel_id=14&data={\"uid\":96023077,\"access_key\":\"ce03007dae8fec024b8482a08e8dda94_sh\"}&device=0000000000000000";
-//	std::cout << hmac_sha256(s, i) << std::endl;;
-//	return 0;
-//}
+int main()
+{	
+	uint8_t outdata[128] = { 0 };
+	uint8_t md[SHA256_DIGESTLEN] = { 0 };
+	//密钥
+	uint8_t secret[] = { "0ebc517adb1b62c6b408df153331f9aa" };
+	//数据
+	uint8_t indata[] = { "app_id=1&channel_id=14&data={\"uid\":96023077,\"access_key\":\"ce03007dae8fec024b8482a08e8dda94_sh\"}&device=0000000000000000" };
+	int len1 = strlen((char*)secret);
+	int len2 = strlen((char*)indata);
+
+	HMAC_SHA256_CTX hmac;
+	hmac_sha256_init(&hmac, secret, len1);
+	hmac_sha256_update(&hmac, indata, len2);
+	hmac_sha256_final(&hmac, md);
+	memcpy(outdata, md, SHA256_DIGESTLEN);
+
+	for (int i = 0; i < SHA256_DIGESTLEN; i++)
+	{
+		printf("%02x", outdata[i]);
+	}
+	printf("\n");
+
+
+	std::string s = "0ebc517adb1b62c6b408df153331f9aa";
+	std::string i = "app_id=1&channel_id=14&data={\"uid\":96023077,\"access_key\":\"ce03007dae8fec024b8482a08e8dda94_sh\"}&device=0000000000000000";
+	std::cout << hmac_sha256(s, i) << std::endl;
+	return 0;
+}
