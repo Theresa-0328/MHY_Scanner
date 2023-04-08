844b750e24698c60b02b3d82edc49220c7601bd0
Theresa-0328 <Theresa-Apocalypse@outlook.com>
2023-04-09 02:37:52 +0800

    bh3 login achieve

--- src/Honkai3StreamQRCode/Mihoyosdk.h
+++ src/Honkai3StreamQRCode/Mihoyosdk.h
@@ -9,13 +9,14 @@
 class Mihoyosdk
 {
 public:
-	std::string verify(std::string uid, std::string access_key);
+	std::string verify(int uid, std::string access_key);
 private:
 	std::string makeSign(std::string);
 	std::string bh3Sign(std::string);
 private:
 	utils u;
-	const std::string verifyData = "{\"uid\":1, \"access_key\" :590}";
-	const std::string verifyBody = "{\"device\":\"0000000000000000\", \"app_id\" : 1, \"channel_id\" : 14, \"data\" : {}, \"sign\" : \"\"}";
+	const std::string loginV2Url = "https://api-sdk.mihoyo.com/bh3_cn/combo/granter/login/v2/login";
+	std::map<std::string, std::string> verifyData = {{"uid","1"},{"access_key","590"}};
+	const std::string verifyBody = "{\"device\":\"0000000000000000\",\"app_id\":1,\"channel_id\":14,\"data\":\"\",\"sign\":\"\"}";
 };
 
