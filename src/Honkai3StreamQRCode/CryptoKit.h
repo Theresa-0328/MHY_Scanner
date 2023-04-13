9f85199c7b4172c3cb54d4000354078a5273ed05
Theresa-0328 <Theresa-Apocalypse@outlook.com>
2023-04-13 15:45:53 +0800

    fix

--- src/Honkai3StreamQRCode/CryptoKit.h
+++ src/Honkai3StreamQRCode/CryptoKit.h
@@ -4,6 +4,7 @@ class CryptoKit
 {
 public:
 	static std::string rsaEncrypt(std::string message, std::string public_key);
-	static std::string formatRsaPublicKey(const std::string& key);
+	static std::string FormatRsaPublicKey(const std::string& key);
 	static std::string HmacSha256(std::string message, std::string key);
+	static std::string Md5(const std::string& str);
 };
