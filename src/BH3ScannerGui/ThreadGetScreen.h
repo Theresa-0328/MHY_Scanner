ae42f3653849e0904bf21c02694eedc2a1ab01a6
Theresa-0328 <Theresa-Apocalypse@outlook.com>
2023-05-04 16:08:26 +0800

    Update Gui

--- src/BH3ScannerGui/ThreadGetScreen.h
+++ src/BH3ScannerGui/ThreadGetScreen.h
@@ -3,13 +3,16 @@
 #include <QThread>
 #include <QMutex>
 #include "LoginBili.h"
-class ThreadGetScreen  : public QThread, public LoginBili
+class ThreadGetScreen
+	: public QThread
 {
+
 	Q_OBJECT
 public:
 	ThreadGetScreen(QObject* parent);
 	~ThreadGetScreen();
 	bool isExit = true;
+	void biliInit(int uid, std::string access_key, std::string uname);
 	void run();
 
 signals:
@@ -17,4 +20,6 @@ signals:
 private:
 	QMutex m_mux;
 	bool signed_in = true;
+	std::string LoginData;
+	Mihoyosdk m;
 };
