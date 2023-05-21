0cb4bfbda57dcd190ce4029d4e6122121d358c6a
Theresa-0328 <Theresa-Apocalypse@outlook.com>
2023-05-21 20:46:00 +0800

    fix

--- src/Honkai3StreamQRCode/V2api.h
+++ src/Honkai3StreamQRCode/V2api.h
@@ -1,6 +1,5 @@
 ﻿#pragma once
 #include <string>
-#include <map>
 #include"HttpClient.h"
 #include"Json.h"
 
@@ -17,9 +16,4 @@ private:
 	//const std::string V1API = "https://api.live.bilibili.com/xlive/web-room/v1/playUrl/playUrl";
 	const std::string V2API = "https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo";
 	const std::string address = "https://api.live.bilibili.com/room/v1/Room/room_init";
-	std::map<std::string, std::string> headers =
-	{
-		{"User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36(KHTML,likeGecko)Chrome/110.0.0.0Safari/537.36 Edg/110.0.1587.41"},
-		{"Referer","https://live.bilibili.com"}
-	};
 };
