dfc6e081f098e484fde44b2e6754b158384313a1
Theresa-0328 <Theresa-Apocalypse@outlook.com>
2023-06-18 20:12:28 +0800

    update

--- src/ScannerCore/OfficialApi.h
+++ src/ScannerCore/OfficialApi.h
@@ -2,5 +2,10 @@
 #include "HttpClient.h"
 class OfficialApi :public HttpClient
 {
-
+public:
+	void request(std::string ticket);
+private:
+	std::string cookie;
+	std::string hkrpgFirst = "api-sdk.mihoyo.com/hkrpg_cn/combo/panda/qrcode/scan";
+	std::string hk4eFirst = "api-sdk.mihoyo.com//hk4e_cn/combo/panda/qrcode/scan";
 };
