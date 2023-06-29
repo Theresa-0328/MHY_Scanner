398bec80142449725196da058a51afe4ed045d93
Theresa-0328 <Theresa-Apocalypse@outlook.com>
2023-06-29 21:28:05 +0800

    update

--- src/ScannerGui/ThreadStreamProcess.cpp
+++ src/ScannerGui/ThreadStreamProcess.cpp
@@ -94,14 +94,15 @@ void ThreadStreamProcess::serverType0()
 				vp.avCodecContext->height, vp.pFrameBGR->data, vp.pFrameBGR->linesize);
 			cv::Mat img(vp.avCodecContext->height, vp.avCodecContext->width, CV_8UC3, vp.pFrameBGR->data[0]);
 			//待优化：注意到有1280 1980和60帧 30帧
-			cv::Mat crop_img = img(cv::Range(340, 720), cv::Range(732, 1186));
+			// 二维码不一定在屏幕的正中央。
+			//cv::Mat crop_img = img(cv::Range(150, 720), cv::Range(200, 1186));
 			if (!threadsacn.isRunning())
 			{
-				threadsacn.setImg(crop_img);
+				threadsacn.setImg(img);
 #ifdef _DEBUG
 				std::cout << "scan count "<< f++ << std::endl;
-				imshow("Video", crop_img);
-				cv::waitKey(10);
+				imshow("Video", img);
+				cv::waitKey(2);
 #endif
 				threadsacn.start();
 			}
@@ -135,6 +136,84 @@ void ThreadStreamProcess::serverType1()
 	td.downloadInit(url);
 	td.start();
 	Mihoyosdk m;
+	std::string LoginData;
+	LoginData = m.verify(std::stoi(uid), gameToken);
+	m.setUserName(uName);
+	ThreadSacn threadsacn;
+	QThread::msleep(3000);
+	VideoProcessor vp;
+	//错误判断
+	if (vp.OpenVideo("./Cache/output.flv") != 0)
+	{
+
+	}
+	int64_t latestTimestamp = av_gettime_relative();
+
+	//if (vp.avformatContext->streams[vp.index]->start_time != AV_NOPTS_VALUE)
+	//{
+	//	int64_t streamTimestamp = av_rescale_q(vp.avformatContext->streams[vp.index]->start_time,
+	//		vp.avformatContext->streams[vp.index]->time_base, { 1, AV_TIME_BASE });
+	//	if (streamTimestamp > latestTimestamp)
+	//	{
+	//		latestTimestamp = streamTimestamp;
+	//	}
+	//}
+
+	av_seek_frame(vp.avformatContext, -1, latestTimestamp, AVSEEK_FLAG_BACKWARD);
+	int f = 0;
+#ifdef _DEBUG
+	cv::namedWindow("Video");
+#endif // _DEBUG
+	while (true)
+	{
+		//int64_t video_duration_sec = vp.avformatContext->duration / AV_TIME_BASE;
+		//size_t timeInSeconds = av_q2d(vp.avstream->time_base);
+		if (stopStream)
+		{
+#ifdef _DEBUG
+			cv::destroyWindow("Video");
+#endif // _DEBUG
+			break;
+		}
+		int op1 = vp.read();
+		if (vp.avPacket->stream_index != vp.index)
+		{
+			continue;
+		}
+		if (vp.SendPacket() != 0)
+		{
+			av_seek_frame(vp.avformatContext, -1, latestTimestamp, AVSEEK_FLAG_BACKWARD);
+			QThread::msleep(300);
+		}
+		{
+			vp.ReceiveFrame(vp.avframe);
+			// 转换像素格式
+			sws_scale(vp.swsCtx, vp.avframe->data, vp.avframe->linesize, 0,
+				vp.avCodecContext->height, vp.pFrameBGR->data, vp.pFrameBGR->linesize);
+			cv::Mat img(vp.avCodecContext->height, vp.avCodecContext->width, CV_8UC3, vp.pFrameBGR->data[0]);
+			//待优化：注意到有1280 1980和60帧 30帧
+			// 二维码不一定在屏幕的正中央。
+			//cv::Mat crop_img = img(cv::Range(150, 720), cv::Range(200, 1186));
+			if (!threadsacn.isRunning())
+			{
+				threadsacn.setImg(img);
+#ifdef _DEBUG
+				std::cout << "scan count " << f++ << std::endl;
+				imshow("Video", img);
+				cv::waitKey(2);
+#endif
+				threadsacn.start();
+			}
+		}
+		if (threadsacn.uqrcode.find("biz_key=bh3_cn") != std::string::npos)
+		{
+			int code = m.scanCheck(threadsacn.getTicket(), LoginData);
+			emit loginResults(code == 0);
+			continue;
+		}
+		av_packet_unref(vp.avPacket);
+	}
+	td.stop();
 }
 
 void ThreadStreamProcess::stop()
@@ -155,5 +234,8 @@ void ThreadStreamProcess::run()
 		serverType1();
 		return;
 	}
-	return;
+	else
+	{
+		return;
+	}
 }
