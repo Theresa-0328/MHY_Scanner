7c954a6d67c963e6da565ad0597f0d4e37d71275
Theresa-0328 <Theresa-Apocalypse@outlook.com>
2023-10-03 13:36:09 +0800

    Login confirmation completed.

--- src/MHY_Scanner_Gui/QRCodeForStream.h
+++ src/MHY_Scanner_Gui/QRCodeForStream.h
@@ -10,10 +10,12 @@ extern "C"
 };
 #include <QThread>
 #include <QMutex>
+#include <Json.h>
 
 #include "OfficialApi.h"
 #include "Mihoyosdk.h"
 #include "Common.h"
+#include "ConfigDate.h"
 
 class ThreadStreamProcess : public QThread
 {
@@ -28,18 +30,22 @@ public:
 	auto init() -> bool;
 	void run();
 	void stop();
+	bool continueLogin = false;
 signals:
 	void loginResults(const ScanRet::Type ret);
+	void loginConfirm(const GameType::Type gameType, bool b);
 private:
 	OfficialApi o;
 	Mihoyosdk m;
 	std::string m_url;
 	void LoginOfficial();
 	void LoginBH3BiliBili();
+	void continueLastLogin();
 	QMutex m_mux;
 	std::string uid;
 	std::string gameToken;
 	std::string m_name;
+	ConfigDate* m_config;
 	ServerType::Type servertype = ServerType::Type::UNKNOW;
 	ScanRet::Type ret = ScanRet::Type::UNKNOW;
 	AVDictionary* pAvdictionary;
