160e887e132716badf50e0f87792323e57d6a433
Theresa-0328 <Theresa-Apocalypse@outlook.com>
2023-11-16 16:07:11 +0800

    fix

--- src/MHY_Scanner_Gui/About_QDialog.cpp
+++ src/MHY_Scanner_Gui/About_QDialog.cpp
@@ -5,7 +5,8 @@ AboutDialog::AboutDialog(QDialog* parent)
 {
 	setFixedSize(400, 200);
 	QVBoxLayout* layout = new QVBoxLayout(this);
-	QLabel* label = new QLabel("", this);
+	QLabel* label = new QLabel("MHY_Scanner \n\n" "版本" SCAN_VER, this);
 	layout->addWidget(label);
+	layout->addWidget(label, 0, Qt::AlignCenter);
 	setLayout(layout);
 }
