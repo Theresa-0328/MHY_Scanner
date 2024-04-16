887b021ec93017485c2cbd8422d0fec4339297c2
Theresa-0328 <Theresa-Apocalypse@outlook.com>
2024-04-16 19:38:57 +0800

    add unit test

--- tests/test_gui.cpp
+++ tests/test_gui.cpp
@@ -2,44 +2,54 @@
 
 #include "ThreadSacn.h"
 
-TEST(ThreadSacn, Test1)
+TEST(ThreadSacn, Test_1440)
 {
-	cv::Mat image = cv::imread(R"(../doc/image/bh3_qrcode_1080.png)");
-	ThreadSacn t;
-	t.setImg(image);
-	t.start();
-	cv::waitKey(100);
-	t.stop();
-	t.wait();
-	const std::string& qrcode1 = t.getQRcode();
-	const std::string& qrcode2 = R"(https://user.mihoyo.com/qr_code_in_game.html?app_id=1&app_name=%E5%B4%A9%E5%9D%8F3&bbs=true&biz_key=bh3_cn&expire=1683143216&ticket=644c22b06fd4de50d3fd5e36)";
-	EXPECT_EQ(qrcode1, qrcode2);
+    cv::Mat image = cv::imread(R"(../doc/image/1.png)");
+    QRScanner qrs;
+    std::string qrcode1;
+    qrs.decodeSingle(image, qrcode1);
+    const std::string& qrcode2 = R"(https://user.mihoyo.com/qr_code_in_game.html?app_id=4&app_name=%E5%8E%9F%E7%A5%9E&bbs=true&biz_key=hk4e_cn&expire=1713656071&ticket=661db98757e55304f24d8fa2)";
+    EXPECT_EQ(qrcode1, qrcode2);
 }
 
-TEST(ThreadSacn, Test2)
+TEST(ThreadSacn, Test_bh3_qrcode_1080)
 {
-	cv::Mat image = cv::imread(R"(../doc/image/hk4e_qrcode_1080.png)");
-	ThreadSacn t;
-	t.setImg(image);
-	t.start();
-	cv::waitKey(100);
-	t.stop();
-	t.wait();
-	const std::string& qrcode1 = t.getQRcode();
-	const std::string& qrcode2 = R"(https://user.mihoyo.com/qr_code_in_game.html?app_id=4&app_name=%E5%8E%9F%E7%A5%9E&bbs=true&biz_key=hk4e_cn&expire=1690964505&ticket=64c37a9907698423ab04edd4)";
-	EXPECT_EQ(qrcode1, qrcode2);
+    cv::Mat image = cv::imread(R"(../doc/image/bh3_qrcode_1080.png)");
+    ThreadSacn t;
+    t.setImg(image);
+    t.start();
+    cv::waitKey(100);
+    t.stop();
+    t.wait();
+    const std::string& qrcode1 = t.getQRcode();
+    const std::string& qrcode2 = R"(https://user.mihoyo.com/qr_code_in_game.html?app_id=1&app_name=%E5%B4%A9%E5%9D%8F3&bbs=true&biz_key=bh3_cn&expire=1683143216&ticket=644c22b06fd4de50d3fd5e36)";
+    EXPECT_EQ(qrcode1, qrcode2);
 }
 
-TEST(ThreadSacn, Test3)
+TEST(ThreadSacn, Test2_hk4e_qrcode_1080)
 {
-	cv::Mat image = cv::imread(R"(../doc/image/hkrpg_qrcode_1080.png)");
-	ThreadSacn t;
-	t.setImg(image);
-	t.start();
-	cv::waitKey(100);
-	t.stop();
-	t.wait();
-	const std::string& qrcode1 = t.getQRcode();
-	const std::string& qrcode2 = R"(https://user.mihoyo.com/qr_code_in_game.html?app_id=8&app_name=%E5%B4%A9%E5%9D%8F%3A%E6%98%9F%E7%A9%B9%E9%93%81%E9%81%93&bbs=true&biz_key=hkrpg_cn&expire=1686822569&ticket=6484472959a1066e0648ae67)";
-	EXPECT_EQ(qrcode1, qrcode2);
+    cv::Mat image = cv::imread(R"(../doc/image/hk4e_qrcode_1080.png)");
+    ThreadSacn t;
+    t.setImg(image);
+    t.start();
+    cv::waitKey(100);
+    t.stop();
+    t.wait();
+    const std::string& qrcode1 = t.getQRcode();
+    const std::string& qrcode2 = R"(https://user.mihoyo.com/qr_code_in_game.html?app_id=4&app_name=%E5%8E%9F%E7%A5%9E&bbs=true&biz_key=hk4e_cn&expire=1690964505&ticket=64c37a9907698423ab04edd4)";
+    EXPECT_EQ(qrcode1, qrcode2);
+}
+
+TEST(ThreadSacn, Test3_hkrpg_qrcode_1080)
+{
+    cv::Mat image = cv::imread(R"(../doc/image/hkrpg_qrcode_1080.png)");
+    ThreadSacn t;
+    t.setImg(image);
+    t.start();
+    cv::waitKey(100);
+    t.stop();
+    t.wait();
+    const std::string& qrcode1 = t.getQRcode();
+    const std::string& qrcode2 = R"(https://user.mihoyo.com/qr_code_in_game.html?app_id=8&app_name=%E5%B4%A9%E5%9D%8F%3A%E6%98%9F%E7%A9%B9%E9%93%81%E9%81%93&bbs=true&biz_key=hkrpg_cn&expire=1686822569&ticket=6484472959a1066e0648ae67)";
+    EXPECT_EQ(qrcode1, qrcode2);
 }
