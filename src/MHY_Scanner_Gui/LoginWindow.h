a87879a56cca8ae2f3fb5664b1f858503087eaec
Theresa-0328 <Theresa-Apocalypse@outlook.com>
2024-05-11 11:00:56 +0800

    update ui

--- src/MHY_Scanner_Gui/LoginWindow.h
+++ src/MHY_Scanner_Gui/LoginWindow.h
@@ -1,28 +1,38 @@
 ﻿#pragma once
 
+#include <thread>
+
 #include <QDialog>
+#include <QLabel>
+#include <QImage>
+#include <QPainter>
 
 #include "ui_LoginWindow.h"
 
 class LoginWindow : public QDialog
 {
-	Q_OBJECT
+    Q_OBJECT
 public:
-	LoginWindow(QDialog* Dialog = nullptr);
-	void getAccountPwd(std::string& Pwd, std::string& Account);
-	std::string cookie;
-	int type = 0;
-	bool getIsReject();
+    LoginWindow(QDialog* Dialog = nullptr);
+    void getAccountPwd(std::string& Pwd, std::string& Account);
+    std::string cookie;
+    int type = 0;
+    bool getIsReject();
 public slots:
-	void showPassword(int state);
-	virtual void accept();
-	virtual void reject();
-	void officialLogin();
-	void officialreject();
+    void showPassword(int state);
+    virtual void accept();
+    virtual void reject();
+    void officialLogin();
+    void officialreject();
+    void controlQRCodeImage(int index);
+
 private:
-	Ui::Dialog ui;
-	void ClearInputBox();
-	std::string Pwd;
-	std::string Account;
-	bool isReject = false;
+    Ui::Dialog ui;
+    void ClearInputBox();
+    std::string Pwd;
+    std::string Account;
+    bool isReject = false;
+    QWidget* widget;
+    QImage QRCodeImage;
+    QLabel* label;
 };
