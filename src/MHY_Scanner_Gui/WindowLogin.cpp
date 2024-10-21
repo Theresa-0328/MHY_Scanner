c271a91990a83c61eab143658d059b743d83b38c
Theresa-0328 <Theresa-Apocalypse@outlook.com>
2024-10-21 12:25:31 +0800

    Rename file

---
+++ src/MHY_Scanner_Gui/WindowLogin.cpp
@@ -0,0 +1,14 @@
+﻿#include "WindowLogin.h"
+
+WindowLogin::WindowLogin(QWidget* parent) :
+    QWidget(parent)
+{
+    //setWindowFlags(Qt::Dialog);
+    setWindowModality(Qt::WindowModal);
+    setMinimumSize(QSize(450, 450));
+    setMaximumSize(QSize(450, 450));
+}
+
+WindowLogin::~WindowLogin()
+{
+}
