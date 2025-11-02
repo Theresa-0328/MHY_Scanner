f9da2212178dd37ce8f5ea70d0eca840c09dd83f
Theresa-0328 <Theresa-Apocalypse@outlook.com>
2025-11-02 09:22:12 +0800

    fix

--- src/Core/Common.h
+++ src/Core/Common.h
@@ -44,7 +44,7 @@ constexpr const char* game_bili_rsa{ "https://line1-sdk-center-login-sh.biligame
 constexpr const char* live_bili_room_init{ "https://api.live.bilibili.com/room/v1/Room/room_init" };
 //constexpr const char* live_bili_v1_playUrl {"https://api.live.bilibili.com/xlive/web-room/v1/playUrl/playUrl";//弃用v1 api
 constexpr const char* live_bili_v2_PlayInfo{ "https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo" };
-constexpr const char* live_douyin_room{ "https://live.douyin.com/webcast/room/web/enter/?aid=6383&live_id=1&device_platform=web&language=zh-CN&enter_from=web_live&cookie_enabled=true&screen_width=1536&screen_height=864&browser_language=zh-CN&browser_platform=Win32&browser_name=Chrome&browser_version=94.0.4606.81&room_id_str=&enter_source=&a_bogus=0&web_rid=" };
+constexpr const char* live_douyin_room{ "https://live.douyin.com/webcast/room/web/enter/?" };
 constexpr const char* live_huya{ "https://m.huya.com" };
 constexpr const char* live_huya_anonymous_uid{ "https://udblgn.huya.com/web/anonymousLogin" };
 
